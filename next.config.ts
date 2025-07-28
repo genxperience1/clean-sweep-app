4f8e8c2de7868761b88e457f793f8c54defb4ad8
genxperience1 <jackietormohlen1@gmail.com>
2025-07-28 01:08:11 -0400

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,29 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+        '*.cloudworkstations.dev',
+        '*.firebase.studio'
+    ]
+  },
+};
+
+export default nextConfig;
